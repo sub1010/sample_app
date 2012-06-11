fde77d4e93ccf697680ac0e18a0397dcc8d3dd4a
Subal Charla <subalcharla@gmail.com>
2012-06-11 19:53:34 +0530

    add contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "Static pages" do
   
+  let (:base_title) {"Ruby on Rails Tutorial Sample App"}
+  
     describe "Home page" do
      
         it "should have the content 'Sample App'" do  
@@ -12,7 +14,7 @@ describe "Static pages" do
         it "should have the right title" do
                         visit '/static_pages/home'
                         page.should have_selector('title', 
-                      :text => "Ruby on Rails Tutorial Sample App | Home")   
+                      :text => "#{base_title} | Home")   
         end
     
     end
@@ -28,7 +30,7 @@ describe "Static pages" do
         it "should have the right title" do
           visit '/static_pages/help' 
           page.should have_selector('title',
-            :text => "Ruby on Rails Tutorial Sample App | Help")
+            :text => "#{base_title} | Help")
         
         end
     
@@ -46,13 +48,26 @@ describe "Static pages" do
       it "should have the right title" do
             visit '/static_pages/about'
             page.should have_selector('title',
-            :text => "Ruby on Rails Tutorial Sample App | About Us")
+            :text => "#{base_title} | About Us")
           end
        
       
     end
  
-    
+    describe "Contact page" do
+      
+      it "should have the content 'Contact'" do
+        visit '/static_pages/contact'
+        page.should have_selector('h1', :text=> 'Contact')
+      end
+      
+      it "should have the right title" do
+        visit '/static_pages/contact'
+        page.should have_selector('title', :text => "#{base_title} | Contact")
+      end
+      
+    end
+      
         
 end
 
