613c93b1b854af74f8d9e4cd1c803825089cb61e
Subal Charla <subalcharla@gmail.com>
2012-07-07 00:18:23 +0530

    FInish user edit, update, index, and destroy actions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120704123654) do
+ActiveRecord::Schema.define(:version => 20120706103254) do
+
+  create_table "microposts", :force => true do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "microposts", ["user_id", "created_at"], :name => "index_microposts_on_user_id_and_created_at"
 
   create_table "tweets", :force => true do |t|
     t.string   "belng_to"
---
+++ spec/models/micropost_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe Micropost do
+  
+  let(:user) { FactoryGirl.create(:user) }
+  before do
+    @micropost = user.microposts.build(content: "Lorem ipsum")
+  end
+  
+  subject { @micropost }
+  
+  it { should respond_to(:content) }
+  it { should respond_to(:user_id) }
+  
+  it { should be_valid }
+  
+  describe "when user_id is not present" do
+    before { @micropost.user_id = nil }
+    it { should_not be valid }
+  end
+end
